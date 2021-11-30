3f4e1e77c542ee8309b0bb72ac8da45e8299da07
sojinkim <201933043@yiu.ac.kr>
2021-11-30 15:17:50 +0900

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkyDog.Models
+{
+    public class User : Notifier
+    {
+        public User(String userName)
+        {
+            Name = userName;
+        }
+
+        private string _Name;
+
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkyDog.Models;
+
+namespace WalkyDog.ViewModels
+{
+    internal class UserViewModel
+    {
+
+        public UserViewModel()
+        {
+            _User = new User("Sojin");
+        }
+
+        private User _User;
+
+        public User User
+        {
+            get
+            {
+                return _User;
+            }
+        }
+
+        public void SvaeChanges()
+        {
+            Debug.Assert(false, String.Format("{0} was a updated.", User.Name));
+        }
+
+    }
+
+
+
+
+
+}
+
