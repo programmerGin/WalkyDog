f5ea735cdd7c9af25c344a0e42fca1a7b238bf94
sojinkim <201933043@yiu.ac.kr>
2021-12-10 18:01:18 +0900

    EDIT 로그인후 페이지 이동 성공

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +25,7 @@ namespace WalkyDog.Views
         public Login()
         {
             InitializeComponent();
-            //DataContext = new LoginViewModel();
+            DataContext = new LoginViewModel(this);
         }
 
       
@@ -42,9 +43,6 @@ private void Loginbtn(object sender, RoutedEventArgs e)
         {
             
 
-          NavigationService.Navigate(
-               new Uri("Views/Matching.xaml", UriKind.Relative)
-               );
         }
     }
 }
